24bbb0e6fb93788de2e266cf0af9e87ace60c493
Andrii Klymchuk <qqvalarqq@gmail.com>
2023-12-15 14:09:45 +0200

    HT_16: added realized the home tasks

---
+++ HT_16/task_1.py
@@ -0,0 +1,260 @@
+# Автоматизувати процес замовлення робота за допомогою Selenium
+# 1. Отримайте та прочитайте дані з "https://robotsparebinindustries.com/orders.csv". Увага! Файл має бути прочитаний
+#    з сервера кожного разу при запускі скрипта, не зберігайте файл локально.
+# 2. Зайдіть на сайт "https://robotsparebinindustries.com/"
+# 3. Перейдіть у вкладку "Order your robot"
+# 4. Для кожного замовлення з файлу реалізуйте наступне:
+#     - закрийте pop-up, якщо він з'явився. Підказка: не кожна кнопка його закриває.
+#     - оберіть/заповніть відповідні поля для замовлення
+#     - натисніть кнопку Preview та збережіть зображення отриманого робота. Увага! Зберігати треба тільки зображення
+#       робота, а не всієї сторінки сайту.
+#     - натисніть кнопку Order та збережіть номер чеку. Увага! Інколи сервер тупить і видає помилку, але повторне
+#       натискання кнопки частіше всього вирішує проблему. Дослідіть цей кейс.
+#     - переіменуйте отримане зображення у формат <номер чеку>_robot (напр. 123456_robot.jpg). Покладіть зображення
+#       в директорію output (яка має створюватися/очищатися під час запуску скрипта).
+#     - замовте наступного робота (шляхом натискання відповідної кнопки)
+#
+# ** Додаткове завдання (необов'язково)
+#     - окрім збереження номеру чеку отримайте також HTML-код всього чеку
+#     - збережіть отриманий код в PDF файл
+#     - додайте до цього файлу отримане зображення робота (бажано на одній сторінці, але не принципово)
+#     - збережіть отриманий PDF файл у форматі <номер чеку>_robot в директорію output. Окремо зображення робота
+#       зберігати не потрібно. Тобто замість зображень у вас будуть pdf файли які містять зображення з чеком.
+import csv
+import os
+import shutil
+from dataclasses import dataclass
+from pathlib import Path
+from random import randrange
+from typing import Tuple
+
+import requests
+from PIL import Image
+from selenium import webdriver
+from selenium.common import TimeoutException, WebDriverException, NoSuchElementException
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from xhtml2pdf import pisa
+
+
+@dataclass
+class RobotOrderInputData:
+    order_number: str
+    head: str
+    body: str
+    legs: str
+    address: str
+
+
+@dataclass
+class RobotOrderReceipt:
+    id: str
+    content: str
+
+
+class RobotSpareBin:
+    RESULT_DIR = Path(Path.cwd(), 'output')
+
+    BASE_URL = 'https://robotsparebinindustries.com'
+    ORDERS_URL = 'https://robotsparebinindustries.com/orders.csv'
+
+    ACTION_TIMEOUT = 1
+    WAIT_TIMEOUT = 15
+
+    DEFAULT_SERVICE_ARGS = [
+        '--allow-running-insecure',
+
+        '--no-sandbox',
+        '--hide-scrollbars',
+        '--disable-infobars',
+
+        '--disable-application-cache',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--disable-notifications',
+        '--disable-setuid-sandbox',
+        '--disable-web-security',
+    ]
+
+    def __init__(self):
+        chrome_options = ChromeOptions()
+
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        chrome_options.add_experimental_option(
+            'prefs', {
+                'profile.default_content_setting_values.notifications': 2,
+                'profile.default_content_settings.popups': 0,
+            })
+        for arg in self.DEFAULT_SERVICE_ARGS:
+            chrome_options.add_argument(arg)
+
+        self.__driver = webdriver.Chrome(service=ChromService(ChromeDriverManager().install()), options=chrome_options)
+        self.__driver.maximize_window()
+
+    def create_results_dir(self):
+        if self.RESULT_DIR.is_dir():
+            shutil.rmtree(self.RESULT_DIR)
+        self.RESULT_DIR.mkdir(parents=True, exist_ok=True)
+
+    def wait_until(self, timeout: int = ACTION_TIMEOUT):
+        action = webdriver.ActionChains(self.__driver)
+        action.pause(randrange(timeout, timeout + 2, 1))
+        action.perform()
+
+    def get_orders_data(self):
+        request = requests.get(self.ORDERS_URL)
+        request.raise_for_status()
+        rows = csv.reader(request.text.splitlines()[1:])
+        return [RobotOrderInputData(*row) for row in rows]
+
+    def wait_locator(self, locator: Tuple[str, str], is_click: bool = True):
+        driver_wait = WebDriverWait(self.__driver, self.WAIT_TIMEOUT)
+        element = driver_wait.until(EC.presence_of_element_located(locator))
+        if is_click:
+            element.click()
+
+    def open_order_page(self):
+        self.__driver.get(self.BASE_URL)
+        self.wait_locator((By.LINK_TEXT, "Order your robot!"))
+        self.wait_until()
+
+    def do_order(self, order_data: RobotOrderInputData):
+        try:
+            self.wait_locator((By.CSS_SELECTOR, 'button.btn-dark'))
+            print('order tab open')
+
+            self.set_order_fields(order_data)
+            print('order fields set')
+
+            preview_img_file = Path(self.RESULT_DIR, f'tmp_{order_data.order_number}_robot.png')
+            self.save_preview_image(preview_img_file)
+            print('get preview imag')
+
+            receipt = self.get_receipt()
+            self.save_receipt_to_pdf(receipt, preview_img_file)
+            print(f'new receipt {receipt.id} save done')
+        except (TypeError, TimeoutException, WebDriverException, NoSuchElementException):
+            print(f'invalid create order with order data number: {order_data.order_number}')
+
+    def set_order_fields(self, order_data: RobotOrderInputData):
+        header_input = self.__driver.find_element(By.NAME, 'head')
+        body_input = self.__driver.find_element(By.ID, f'id-body-{order_data.body}')
+        legs_input = self.__driver.find_element(By.XPATH, '//input[@class="form-control"][@type="number"]')
+        address_input = self.__driver.find_element(By.XPATH, '//input[@class="form-control"][@type="text"]')
+
+        header_input = Select(header_input)
+        header_input.select_by_value(str(order_data.head))
+        self.wait_until()
+
+        legs_input.send_keys(order_data.legs)
+        self.wait_until()
+
+        address_input.send_keys(order_data.address)
+        self.wait_until()
+
+        body_input.click()
+        self.wait_until()
+
+    def open_preview_image(self):
+        preview_btn = self.__driver.find_element(By.ID, 'preview')
+        preview_btn.click()
+        self.wait_locator((By.ID, 'robot-preview-image'), False)
+
+    def save_preview_image(self, file: Path):
+        self.open_preview_image()
+
+        head_img_url = self.__driver.find_element(By.XPATH, '//img[@alt="Head"]').get_attribute('src')
+        body_img_url = self.__driver.find_element(By.XPATH, '//img[@alt="Body"]').get_attribute('src')
+        legs_img_url = self.__driver.find_element(By.XPATH, '//img[@alt="Legs"]').get_attribute('src')
+
+        head_img = Image.open(requests.get(head_img_url, stream=True).raw)
+        body_img = Image.open(requests.get(body_img_url, stream=True).raw)
+        legs_img = Image.open(requests.get(legs_img_url, stream=True).raw)
+
+        img_width = max(head_img.size[0], body_img.size[0], legs_img.size[0])
+        img_height = head_img.size[1] + body_img.size[1] + legs_img.size[1]
+
+        img = Image.new("RGBA", (img_width, img_height))
+        img.paste(head_img, (int((img_width - head_img.size[0]) / 2), 0))
+        img.paste(body_img, (int((img_width - body_img.size[0]) / 2), head_img.size[1]))
+        img.paste(legs_img, (int((img_width - legs_img.size[0]) / 2), head_img.size[1] + body_img.size[1]))
+
+        img.save(file)
+
+    def order_btn_click(self):
+        order_btn = self.__driver.find_element(By.ID, 'order')
+        order_btn.click()
+
+    def is_alert_danger(self) -> bool:
+        try:
+            self.__driver.find_element(By.CLASS_NAME, 'alert-danger')
+            return True
+        except (WebDriverException, NoSuchElementException):
+            return False
+
+    def is_order_successful(self) -> bool:
+        try:
+            self.wait_locator((By.ID, 'receipt'), False)
+            return True
+        except (WebDriverException, NoSuchElementException, TimeoutException):
+            return not self.is_alert_danger()
+
+    def get_receipt(self) -> RobotOrderReceipt:
+        self.order_btn_click()
+
+        while not self.is_order_successful():
+            self.order_btn_click()
+
+        id = self.__driver.find_element(By.CLASS_NAME, 'badge-success').text
+        id = id[id.rfind('-') + 1:]
+
+        receipt_element = self.__driver.find_element(By.ID, 'receipt')
+        receipt_html = receipt_element.get_attribute('innerHTML')
+
+        return RobotOrderReceipt(id=id, content=receipt_html)
+
+    def save_receipt_to_pdf(self, order: RobotOrderReceipt, preview_img_file: Path):
+        content = f'{order.content}'
+
+        if os.path.exists(preview_img_file):
+            content += f'<img src="{preview_img_file}"/>'
+
+        file = Path(self.RESULT_DIR, f'{order.id}_robot.pdf')
+        with open(file, "w+b") as pdf_file:
+            pisa.CreatePDF(content, pdf_file)
+            pdf_file.close()
+
+        if os.path.exists(preview_img_file):
+            os.remove(preview_img_file)
+
+    def another_order_btn_click(self):
+        order_btn = self.__driver.find_element(By.ID, 'order-another')
+        order_btn.click()
+
+    def start(self):
+        try:
+            self.create_results_dir()
+            orders_data = self.get_orders_data()
+            print('get orders input data done')
+            self.wait_until()
+
+            self.open_order_page()
+            for order_data in orders_data:
+                self.do_order(order_data)
+                self.another_order_btn_click()
+        except Exception as e:
+            print(f'Exception: {e}')
+        else:
+            print('Done')
+        finally:
+            self.__driver.close()
+            self.__driver.quit()
+
+
+robot = RobotSpareBin()
+robot.start()
---
+++ HT_16/task_1_custom.py
@@ -0,0 +1,287 @@
+# Покрито катомним Exception
+import csv
+import os
+import shutil
+from dataclasses import dataclass
+from pathlib import Path
+from random import randrange
+from typing import Tuple
+
+import requests
+from PIL import Image
+from selenium import webdriver
+from selenium.common import TimeoutException, WebDriverException, NoSuchElementException
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from xhtml2pdf import pisa
+
+
+@dataclass
+class RobotOrderInputData:
+    order_number: str
+    head: str
+    body: str
+    legs: str
+    address: str
+
+
+@dataclass
+class RobotOrderReceipt:
+    id: str
+    content: str
+
+
+class RobotSpareBinException(Exception):
+    pass
+
+
+class RobotSpareBin:
+    RESULT_DIR = Path(Path.cwd(), 'output')
+
+    BASE_URL = 'https://robotsparebinindustries.com'
+    ORDERS_URL = 'https://robotsparebinindustries.com/orders.csv'
+
+    ACTION_TIMEOUT = 3
+    WAIT_TIMEOUT = 15
+
+    DEFAULT_SERVICE_ARGS = [
+        '--allow-running-insecure',
+
+        '--no-sandbox',
+        '--hide-scrollbars',
+        '--disable-infobars',
+
+        '--disable-application-cache',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--disable-notifications',
+        '--disable-setuid-sandbox',
+        '--disable-web-security',
+    ]
+
+    def __init__(self):
+        chrome_options = ChromeOptions()
+
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        chrome_options.add_experimental_option(
+            'prefs', {
+                'profile.default_content_setting_values.notifications': 2,
+                'profile.default_content_settings.popups': 0,
+            })
+        for arg in self.DEFAULT_SERVICE_ARGS:
+            chrome_options.add_argument(arg)
+
+        self.__driver = webdriver.Chrome(service=ChromService(ChromeDriverManager().install()), options=chrome_options)
+        self.__driver.maximize_window()
+
+    def create_results_dir(self):
+        try:
+            if self.RESULT_DIR.is_dir():
+                shutil.rmtree(self.RESULT_DIR)
+            self.RESULT_DIR.mkdir(parents=True, exist_ok=True)
+        except Exception:
+            raise RobotSpareBinException('invalid results dir created')
+
+    def remove_file(self, file):
+        try:
+            if file and os.path.exists(file):
+                os.remove(file)
+        except Exception:
+            raise RobotSpareBinException(f'invalid remove file {file}')
+
+    def wait_until(self, timeout: int = ACTION_TIMEOUT):
+        action = webdriver.ActionChains(self.__driver)
+        action.pause(randrange(timeout, timeout + 5, 1))
+        action.perform()
+
+    def get_orders_data(self):
+        try:
+            request = requests.get(self.ORDERS_URL)
+            request.raise_for_status()
+            rows = csv.reader(request.text.splitlines()[1:])
+            return [RobotOrderInputData(*row) for row in rows]
+        except requests.HTTPError:
+            raise RobotSpareBinException('invalid request')
+        except Exception:
+            raise RobotSpareBinException('invalid get orders data')
+
+    def wait_locator(self, locator: Tuple[str, str], is_click: bool = True):
+        driver_wait = WebDriverWait(self.__driver, self.WAIT_TIMEOUT)
+        element = driver_wait.until(EC.presence_of_element_located(locator))
+        if is_click:
+            element.click()
+
+    def open_order_page(self):
+        try:
+            self.__driver.get(self.BASE_URL)
+            self.wait_locator((By.LINK_TEXT, "Order your robot!"))
+        except (WebDriverException, NoSuchElementException):
+            raise RobotSpareBinException('invalid open order page')
+        else:
+            self.wait_until()
+
+    def do_order(self, order_data: RobotOrderInputData):
+        try:
+            self.wait_locator((By.CSS_SELECTOR, 'button.btn-dark'))
+            print('order tab open')
+
+            self.set_order_fields(order_data)
+            print('order fields set')
+
+            preview_img_file = Path(self.RESULT_DIR, f'tmp_{order_data.order_number}_robot.png')
+            self.save_preview_image(preview_img_file)
+            print('get preview imag')
+
+            receipt = self.get_receipt()
+            self.save_receipt_to_pdf(receipt, preview_img_file)
+            print(f'new receipt {receipt.id} save done')
+        except RobotSpareBinException as e:
+            print(f'invalid create order with order data number {order_data.order_number}: {e}')
+
+    def set_order_fields(self, order_data: RobotOrderInputData):
+        try:
+            header_input = self.__driver.find_element(By.NAME, 'head')
+            body_input = self.__driver.find_element(By.ID, f'id-body-{order_data.body}')
+            legs_input = self.__driver.find_element(By.XPATH, '//input[@class="form-control"][@type="number"]')
+            address_input = self.__driver.find_element(By.XPATH, '//input[@class="form-control"][@type="text"]')
+
+            header_input = Select(header_input)
+            header_input.select_by_value(str(order_data.head))
+            self.wait_until()
+
+            legs_input.send_keys(order_data.legs)
+            self.wait_until()
+
+            address_input.send_keys(order_data.address)
+            self.wait_until()
+
+            body_input.click()
+            self.wait_until()
+        except (WebDriverException, NoSuchElementException):
+            raise RobotSpareBinException('invalid set order fields')
+        else:
+            self.wait_until()
+
+    def open_preview_image(self):
+        try:
+            preview_btn = self.__driver.find_element(By.ID, 'preview')
+            preview_btn.click()
+            self.wait_locator((By.ID, 'robot-preview-image'), False)
+        except (TimeoutException, WebDriverException, NoSuchElementException):
+            raise RobotSpareBinException('invalid get preview image')
+
+    def save_preview_image(self, file: Path):
+        self.open_preview_image()
+
+        try:
+            head_img_url = self.__driver.find_element(By.XPATH, '//img[@alt="Head"]').get_attribute('src')
+            body_img_url = self.__driver.find_element(By.XPATH, '//img[@alt="Body"]').get_attribute('src')
+            legs_img_url = self.__driver.find_element(By.XPATH, '//img[@alt="Legs"]').get_attribute('src')
+
+            head_img = Image.open(requests.get(head_img_url, stream=True).raw)
+            body_img = Image.open(requests.get(body_img_url, stream=True).raw)
+            legs_img = Image.open(requests.get(legs_img_url, stream=True).raw)
+
+            img_width = max(head_img.size[0], body_img.size[0], legs_img.size[0])
+            img_height = head_img.size[1] + body_img.size[1] + legs_img.size[1]
+
+            img = Image.new("RGBA", (img_width, img_height))
+            img.paste(head_img, (int((img_width - head_img.size[0]) / 2), 0))
+            img.paste(body_img, (int((img_width - body_img.size[0]) / 2), head_img.size[1]))
+            img.paste(legs_img, (int((img_width - legs_img.size[0]) / 2), head_img.size[1] + body_img.size[1]))
+
+            img.save(file)
+        except (WebDriverException, NoSuchElementException):
+            raise RobotSpareBinException('invalid parser html for save image')
+        except Exception:
+            raise RobotSpareBinException('invalid save preview image')
+
+    def order_btn_click(self):
+        try:
+            order_btn = self.__driver.find_element(By.ID, 'order')
+            order_btn.click()
+        except (WebDriverException, NoSuchElementException):
+            raise RobotSpareBinException('invalid order button click')
+
+    def is_alert_danger(self) -> bool:
+        try:
+            self.__driver.find_element(By.CLASS_NAME, 'alert-danger')
+            return True
+        except (WebDriverException, NoSuchElementException):
+            return False
+
+    def is_order_successful(self) -> bool:
+        try:
+            self.wait_locator((By.ID, 'receipt'), False)
+            return True
+        except (WebDriverException, NoSuchElementException, TimeoutException):
+            return not self.is_alert_danger()
+
+    def get_receipt(self) -> RobotOrderReceipt:
+        self.order_btn_click()
+
+        while not self.is_order_successful():
+            self.order_btn_click()
+
+        try:
+            id = self.__driver.find_element(By.CLASS_NAME, 'badge-success').text
+            id = id[id.rfind('-') + 1:]
+
+            receipt_element = self.__driver.find_element(By.ID, 'receipt')
+            receipt_html = receipt_element.get_attribute('innerHTML')
+
+            return RobotOrderReceipt(id=id, content=receipt_html)
+        except (WebDriverException, NoSuchElementException):
+            raise RobotSpareBinException('invalid get order')
+
+    def save_receipt_to_pdf(self, order: RobotOrderReceipt, preview_img_file: Path):
+        try:
+            content = f'{order.content}'
+
+            if preview_img_file:
+                content += f'<img src="{preview_img_file}"/>'
+
+            file = Path(self.RESULT_DIR, f'{order.id}_robot.pdf')
+            with open(file, "w+b") as pdf_file:
+                pisa.CreatePDF(content, pdf_file)
+                pdf_file.close()
+        except Exception:
+            raise RobotSpareBinException('invalid save pdf')
+        else:
+            self.remove_file(preview_img_file)
+
+    def another_order_btn_click(self):
+        try:
+            order_btn = self.__driver.find_element(By.ID, 'order-another')
+            order_btn.click()
+        except (WebDriverException, NoSuchElementException):
+            raise RobotSpareBinException('invalid another button click')
+
+    def start(self):
+        try:
+            self.create_results_dir()
+            orders_data = self.get_orders_data()
+            print('get orders input data done')
+            self.wait_until()
+
+            self.open_order_page()
+            for order_data in orders_data:
+                self.do_order(order_data)
+                self.another_order_btn_click()
+
+        except RobotSpareBinException as e:
+            print(f'Exception: {e}')
+        else:
+            print('Done')
+        finally:
+            self.__driver.close()
+            self.__driver.quit()
+
+
+robot = RobotSpareBin()
+robot.start()
