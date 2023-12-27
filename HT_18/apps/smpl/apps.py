e536e2b6ff43d70dbbb2e14881bd9c53f5683ee1
Andrii Klymchuk <qqvalarqq@gmail.com>
2023-12-27 12:20:36 +0200

    HT_18: added bootstrap 5.3 (with sample)

---
+++ HT_18/apps/smpl/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmplConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.smpl'
--- HT_18/settings/main.py
+++ HT_18/settings/main.py
@@ -3,3 +3,9 @@ from settings.base import *
 INSTALLED_APPS += [
 
 ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+TEMPLATES[0]['DIRS'].append('templates')
